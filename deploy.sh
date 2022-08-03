d8a8642c7cf222ca37e8d44656328988a482f2a0
LCY <1398304358@qq.com>
2022-08-03 22:42:56 +0800

    配置

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # 生成静态文件
-# npm run build
+npm run build
 
 # 进入生成的文件夹
-# cd public
+cd public
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -16,7 +16,7 @@ git add -A
 git commit -m 'update: 目录结构'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:Baiyusoup/sprint.git master
+git push -f git@github.com:CoderTwelve/sprint.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
